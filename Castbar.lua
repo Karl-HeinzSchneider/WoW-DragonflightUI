6083e9970821d1ef3553e3d8b9e37f093b76fa7f
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2023-04-11 04:03:25 +0200

    F

--- Castbar.lua
+++ Castbar.lua
@@ -74,25 +74,12 @@ function CreateNewCastbar()
         if percent == 1 then
             f.Spark:Hide()
         else
+            --f.Spark:SetPoint('CENTER', f.Bar, 'LEFT', sizeX / 2, 0 + 15)
             f.Spark:Show()
             local dx = 2
-            f.Spark:SetPoint('CENTER', f.Bar, 'LEFT', percent * sizeX - dx, 0)
+            f.Spark:SetPoint('CENTER', f.Bar, 'LEFT', (value * sizeX) / statusMax, 0 + 15)
         end
     end
-
-    CastingBarFrame:HookScript(
-        'OnValueChanged',
-        function(self)
-            UpdateCastBarValues(self)
-            UpdateSpark(self)
-        end
-    )
-    CastingBarFrame:HookScript(
-        'OnMinMaxChanged',
-        function(self)
-            UpdateCastBarValues(self)
-        end
-    )
 
     local bg = CreateFrame('Frame', 'DragonflightUICastbarNameBackground', CastingBarFrame)
     bg:SetSize(sizeX, sizeY)
@@ -110,15 +97,55 @@ function CreateNewCastbar()
     local text = bg:CreateFontString('DragonflightUICastbarText', 'OVERLAY', 'GameFontHighlight')
     text:SetText('12')
     text:SetPoint('TOP', f, 'BOTTOM', 0, -1)
-    text:SetText('SHADOW BOLT')
-
+    text:SetText('SHADOW BOLT DEBUG')
     f.Text = text
 
-    hooksecurefunc(
+    local textValueMax = bg:CreateFontString('DragonflightUICastbarText', 'OVERLAY', 'GameFontHighlight')
+    textValueMax:SetPoint('TOP', f, 'BOTTOM', 0, -1)
+    textValueMax:SetPoint('RIGHT', f.Background, 'RIGHT', -10, 0)
+    textValueMax:SetText('/ 4.2')
+    f.TextValueMax = textValueMax
+
+    local textValue = bg:CreateFontString('DragonflightUICastbarText', 'OVERLAY', 'GameFontHighlight')
+    textValue:SetPoint('RIGHT', f.TextValueMax, 'LEFT', 0, 0)
+    textValue:SetText('0.69')
+    f.TextValue = textValue
+
+    local UpdateExtratext = function(other)
+        local value = other:GetValue()
+        local statusMin, statusMax = other:GetMinMaxValues()
+
+        if value == statusMax then
+            frame.Castbar.TextValue:SetText('')
+            frame.Castbar.TextValueMax:SetText('')
+        elseif frame.Castbar.bChanneling then
+            f.TextValue:SetText(string.format('%.1f', value))
+            f.TextValueMax:SetText(' / ' .. string.format('%.2f', statusMax))
+        else
+            f.TextValue:SetText(string.format('%.1f', statusMax - value))
+            f.TextValueMax:SetText(' / ' .. string.format('%.2f', statusMax))
+        end
+    end
+
+    --[[     hooksecurefunc(
         CastingBarFrame.Text,
         'SetText',
         function(self)
-            text:SetText(self:GetText())
+            --text:SetText(self:GetText())
+        end
+    ) ]]
+    CastingBarFrame:HookScript(
+        'OnValueChanged',
+        function(self)
+            UpdateCastBarValues(self)
+            UpdateSpark(self)
+            UpdateExtratext(self)
+        end
+    )
+    CastingBarFrame:HookScript(
+        'OnMinMaxChanged',
+        function(self)
+            UpdateCastBarValues(self)
         end
     )
 end
@@ -127,14 +154,34 @@ function UpdateCastbarChanges()
     CastingBarFrame:ClearAllPoints()
     CastingBarFrame:SetPoint('CENTER', UIParent, 'BOTTOM', 0, -300)
     --CastingBarFrame:SetPoint('BOTTOM', UIParent, 'BOTTOM', 0, 500)
-
-    local standardRef = 'Interface\\Addons\\DragonflightUI\\Textures\\Castbar\\CastingBarStandard2'
-    frame.Castbar.Bar:SetStatusBarTexture(standardRef)
 end
 
-function Interrupted()
+function SetBarNormal()
+    local standardRef = 'Interface\\Addons\\DragonflightUI\\Textures\\Castbar\\CastingBarStandard2'
+    frame.Castbar.Bar:SetStatusBarTexture(standardRef)
+
+    frame.Castbar.bChanneling = false
+    local name, text, texture, startTimeMS, endTimeMS, isTradeSkill, castID, notInterruptible, spellId =
+        UnitCastingInfo('player')
+
+    frame.Castbar.Text:SetText(text:sub(1, 15))
+end
+
+function SetBarChannel()
+    local channelRef = 'Interface\\Addons\\DragonflightUI\\Textures\\Castbar\\CastingBarChannel'
+    frame.Castbar.Bar:SetStatusBarTexture(channelRef)
+
+    frame.Castbar.bChanneling = true
+    local name, text, texture, startTimeMS, endTimeMS, isTradeSkill, notInterruptible, spellId =
+        UnitChannelInfo('player')
+    frame.Castbar.Text:SetText(name:sub(1, 15))
+end
+
+function SetBarInterrupted()
     local interruptedRef = 'Interface\\Addons\\DragonflightUI\\Textures\\Castbar\\CastingBarInterrupted2'
     frame.Castbar.Bar:SetStatusBarTexture(interruptedRef)
+
+    frame.Castbar.Text:SetText(CastingBarFrame.Text:GetText())
 end
 
 function CastbarModule()
@@ -158,14 +205,15 @@ Core.RegisterModule(Module, {}, {}, false, CastbarModule)
 
 function frame:OnEvent(event, arg1)
     --print('event', event, arg1)
+    UpdateCastbarChanges()
     if event == 'PLAYER_ENTERING_WORLD' then
-        UpdateCastbarChanges()
     elseif (event == 'UNIT_SPELLCAST_START' and arg1 == 'player') then
-        UpdateCastbarChanges()
+        SetBarNormal()
     elseif (event == 'UNIT_SPELLCAST_INTERRUPTED' and arg1 == 'player') then
-        Interrupted()
+        SetBarInterrupted()
+    elseif (event == 'UNIT_SPELLCAST_CHANNEL_START' and arg1 == 'player') then
+        SetBarChannel()
     else
-        --UpdateCastbarChanges()
     end
 end
 frame:SetScript('OnEvent', frame.OnEvent)
