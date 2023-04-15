a6fc282512a53a614579db59b5689b73e2bd3123
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2023-04-15 20:58:52 +0200

    Create Chat.lua

---
+++ Modules/Chat.lua
@@ -0,0 +1,60 @@
+local DF = LibStub('AceAddon-3.0'):GetAddon('DragonflightUI')
+local mName = 'Chat'
+local Module = DF:NewModule(mName, 'AceConsole-3.0')
+
+local db, getOptions
+
+local defaults = {
+    profile = {
+        scale = 1,
+        dX = 0,
+        dY = 0
+    }
+}
+
+local options = {
+    type = 'group',
+    name = 'DragonflightUI - ' .. mName,
+    args = {
+        toggle = {
+            type = 'toggle',
+            name = 'Enable',
+            get = function()
+                return DF:GetModuleEnabled(mName)
+            end,
+            set = function(info, v)
+                DF:SetModuleEnabled(mName, v)
+            end,
+            order = 1
+        },
+        reload = {
+            type = 'execute',
+            name = '/reload',
+            desc = 'reloads UI',
+            func = function()
+                ReloadUI()
+            end,
+            order = 69
+        }
+    }
+}
+
+function Module:OnInitialize()
+    self:Print('Module ' .. mName .. ' OnInitialize()')
+    self.db = DF.db:RegisterNamespace(mName, defaults)
+    db = self.db.profile
+
+    self:SetEnabledState(DF:GetModuleEnabled(mName))
+    DF:RegisterModuleOptions(mName, options)
+end
+
+function Module:OnEnable()
+    self:Print('Module ' .. mName .. ' OnEnable()')
+end
+
+function Module:OnDisable()
+end
+
+function Module:ApplySettings()
+    db = self.db.profile
+end
