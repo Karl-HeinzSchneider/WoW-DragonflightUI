f64ba0d3004f5a8eeb3e902bc893623423542736
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2023-04-16 01:23:45 +0200

    remove old code

--- Core.lua
+++ Core.lua
@@ -54,31 +54,3 @@ function DF:Debug(m, value)
         m:Print(value)
     end
 end
-
--- Vars
-
-local colorTable = {
-    ['yellow'] = '|c00ffff00',
-    ['green'] = '|c0000ff00',
-    ['orange'] = '|c00ffc400',
-    ['red'] = '|c00ff0000'
-}
-
-local InterfaceVersion = select(4, GetBuildInfo())
-Core.Wrath = InterfaceVersion >= 30400
-Core.Era = InterfaceVersion <= 20000
-
-Core.Modules = {}
-Core.Sub = {}
-Core.RegisterModule = function(name, meta, options, default, func)
-    table.insert(
-        Core.Modules,
-        {
-            ['name'] = name,
-            ['meta'] = meta,
-            ['options'] = options,
-            ['default'] = default,
-            ['func'] = func
-        }
-    )
-end
