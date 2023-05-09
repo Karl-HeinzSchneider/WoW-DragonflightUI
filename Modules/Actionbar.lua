a0870b14840bcaa96472625857f764712bc74116
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2023-05-09 06:23:24 +0200

    Fix UpdateContainerFrameAnchors Hook

--- Modules/Actionbar.lua
+++ Modules/Actionbar.lua
@@ -1558,25 +1558,84 @@ function Module.ChangeBackpack()
 end
 
 function Module.HookBags()
-    hooksecurefunc(
-        'UpdateContainerFrameAnchors',
-        function()
-            -- from '\BlizzardInterfaceCode\Interface\FrameXML\ContainerFrame_Shared.lua'
-            local CONTAINER_WIDTH = 192
+    -- from '\BlizzardInterfaceCode\Interface\FrameXML\ContainerFrame_Shared.lua'
+    local UpdateContainerFrameAnchorsModified = function()
+        --CHANGE
+        local CONTAINER_OFFSET_X = 0
+        local CONTAINER_OFFSET_Y = 92
 
-            local CONTAINER_BASE_HEIGHT = 95
+        local frame, xOffset, yOffset, screenHeight, freeScreenHeight, leftMostPoint, column
+        local screenWidth = GetScreenWidth()
+        local containerScale = 1
+        local leftLimit = 0
+        if (BankFrame:IsShown()) then
+            leftLimit = BankFrame:GetRight() - 25
+        end
 
-            ContainerFrame1:SetPoint('BOTTOMRIGHT', UIParent, 'BOTTOMRIGHT', 0, CONTAINER_BASE_HEIGHT)
-            for i = 2, 5 do
-                local bagRef = _G['ContainerFrame' .. i]
-                local point, relativeTo, relativePoint, xOfs, yOfs = bagRef:GetPoint(1)
-
-                if relativePoint == 'BOTTOMRIGHT' then
-                    bagRef:SetPoint(point, relativeTo, relativePoint, -CONTAINER_WIDTH, CONTAINER_BASE_HEIGHT + 2)
+        while (containerScale > CONTAINER_SCALE) do
+            screenHeight = GetScreenHeight() / containerScale
+            -- Adjust the start anchor for bags depending on the multibars
+            xOffset = CONTAINER_OFFSET_X / containerScale
+            yOffset = CONTAINER_OFFSET_Y / containerScale
+            -- freeScreenHeight determines when to start a new column of bags
+            freeScreenHeight = screenHeight - yOffset
+            leftMostPoint = screenWidth - xOffset
+            column = 1
+            local frameHeight
+            for index, frameName in ipairs(ContainerFrame1.bags) do
+                frameHeight = _G[frameName]:GetHeight()
+                if (freeScreenHeight < frameHeight) then
+                    -- Start a new column
+                    column = column + 1
+                    leftMostPoint = screenWidth - (column * CONTAINER_WIDTH * containerScale) - xOffset
+                    freeScreenHeight = screenHeight - yOffset
                 end
+                freeScreenHeight = freeScreenHeight - frameHeight - VISIBLE_CONTAINER_SPACING
+            end
+            if (leftMostPoint < leftLimit) then
+                containerScale = containerScale - 0.01
+            else
+                break
             end
         end
-    )
+
+        if (containerScale < CONTAINER_SCALE) then
+            containerScale = CONTAINER_SCALE
+        end
+
+        screenHeight = GetScreenHeight() / containerScale
+        -- Adjust the start anchor for bags depending on the multibars
+        xOffset = CONTAINER_OFFSET_X / containerScale
+        yOffset = CONTAINER_OFFSET_Y / containerScale
+        -- freeScreenHeight determines when to start a new column of bags
+        freeScreenHeight = screenHeight - yOffset
+        column = 0
+        for index, frameName in ipairs(ContainerFrame1.bags) do
+            frame = _G[frameName]
+            frame:SetScale(containerScale)
+            if (index == 1) then
+                -- First bag
+                frame:SetPoint('BOTTOMRIGHT', frame:GetParent(), 'BOTTOMRIGHT', -xOffset, yOffset)
+            elseif (freeScreenHeight < frame:GetHeight()) then
+                -- Start a new column
+                column = column + 1
+                freeScreenHeight = screenHeight - yOffset
+                frame:SetPoint(
+                    'BOTTOMRIGHT',
+                    frame:GetParent(),
+                    'BOTTOMRIGHT',
+                    -(column * CONTAINER_WIDTH) - xOffset,
+                    yOffset
+                )
+            else
+                -- Anchor to the previous bag
+                frame:SetPoint('BOTTOMRIGHT', ContainerFrame1.bags[index - 1], 'TOPRIGHT', 0, CONTAINER_SPACING)
+            end
+            freeScreenHeight = freeScreenHeight - frame:GetHeight() - VISIBLE_CONTAINER_SPACING
+        end
+    end
+
+    hooksecurefunc('UpdateContainerFrameAnchors', UpdateContainerFrameAnchorsModified)
 end
 
 function Module.MoveBars()
