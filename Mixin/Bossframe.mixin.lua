25070709011b7c2d336eea530477314470788dcb
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-06-01 05:51:27 +0200

    add type

--- Mixin/Bossframe.mixin.lua
+++ Mixin/Bossframe.mixin.lua
@@ -37,6 +37,16 @@ function DragonflightUIBossframeMixin:SetupTextures()
     border:SetTexCoord(0.392578, 0.841797, 0.00195312, 0.220703)
     border:SetSize(230, 112)
     border:SetPoint('CENTER')
+
+    local bosstype = f:CreateTexture('DragonflightUIBossframeType')
+    f.type = bosstype
+    bosstype:SetDrawLayer('BACKGROUND', 2)
+    bosstype:SetTexture(base)
+    bosstype:SetTexCoord(0.392578, 0.724609, 0.224609, 0.294922)
+    bosstype:SetSize(173, 36)
+    bosstype:SetPoint('TOPLEFT', 4, -14)
+    bosstype:SetVertexColor(1.0, 0, 0, 1.0)
+
 end
 
 function DragonflightUIBossframeMixin:CreateHP(unit)
@@ -44,13 +54,12 @@ function DragonflightUIBossframeMixin:CreateHP(unit)
 
     local hp = CreateFrame('StatusBar', nil, f)
     f.hp = hp
-    hp:SetWidth(85)
-    hp:SetHeight(10)
-    hp:SetPoint("LEFT", f, "LEFT", 3, -1);
-    hp:SetScale(2)
+    hp:SetWidth(170)
+    hp:SetHeight(20)
+    hp:SetPoint("LEFT", f, "LEFT", 5, -2);
 
     hp:SetStatusBarTexture(
-        'Interface\\Addons\\DragonflightUI\\Textures\\Unitframe\\UI-HUD-UnitFrame-Target-PortraitOn-Bar-Health')
+        'Interface\\Addons\\DragonflightUI\\Textures\\Bossframe\\UI-HUD-UnitFrame-Target-Boss-Small-PortraitOff-Bar-Health')
     hp:SetStatusBarColor(1, 1, 1, 1)
 end
 
@@ -59,12 +68,11 @@ function DragonflightUIBossframeMixin:CreateMana(unit)
 
     local mana = CreateFrame('StatusBar', nil, f)
     f.mana = mana
-    mana:SetWidth(85)
-    mana:SetHeight(8)
+    mana:SetWidth(170)
+    mana:SetHeight(16)
     mana:SetPoint("TOPRIGHT", f.hp, "BOTTOMRIGHT", 0, -1);
-    mana:SetScale(2)
 
     mana:SetStatusBarTexture(
-        'Interface\\Addons\\DragonflightUI\\Textures\\Unitframe\\UI-HUD-UnitFrame-Player-PortraitOn-Bar-Mana')
+        'Interface\\Addons\\DragonflightUI\\Textures\\Bossframe\\UI-HUD-UnitFrame-Target-Boss-Small-PortraitOff-Bar-Mana')
     mana:SetStatusBarColor(1, 1, 1, 1)
 end
