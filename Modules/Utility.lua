cbed82f0802b867c9d1cc709b44f6ef68df70e2f
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-06-20 08:56:37 +0200

    Utility: friendsColor

--- Modules/Utility.lua
+++ Modules/Utility.lua
@@ -5,7 +5,7 @@ Module.Tmp = {}
 
 Mixin(Module, DragonflightUIModulesMixin)
 
-local defaults = {profile = {scale = 1, first = {}}}
+local defaults = {profile = {scale = 1, first = {friendsColor = true}}}
 Module:SetDefaults(defaults)
 
 local function getDefaultStr(key, sub)
@@ -33,7 +33,15 @@ local utilityOptions = {
     name = 'Utility',
     get = getOption,
     set = setOption,
-    args = {headerFriends = {type = 'header', name = 'Friendsframe', order = 1}}
+    args = {
+        friendsHeader = {type = 'header', name = 'Friendsframe', order = 10},
+        friendsColor = {
+            type = 'toggle',
+            name = 'Class Color',
+            desc = '' .. getDefaultStr('friendsColor', 'first'),
+            order = 11
+        }
+    }
 }
 
 function Module:OnInitialize()
@@ -48,6 +56,8 @@ end
 function Module:OnEnable()
     DF:Debug(self, 'Module ' .. mName .. ' OnEnable()')
     self:SetWasEnabled(true)
+
+    Module:SetupLookupTable()
 
     if DF.Cata then
         Module.Cata()
@@ -69,7 +79,6 @@ function Module:OnDisable()
 end
 
 function Module:RegisterOptionScreens()
-    if not DF.Cata then return end
     DF.ConfigModule:RegisterOptionScreen('Misc', 'Utility', {
         name = 'Utility',
         sub = 'first',
@@ -93,7 +102,70 @@ function Module:RefreshOptionScreens()
 end
 
 function Module:ApplySettings()
-    local state = Module.db.profile
+    local db = Module.db.profile
+
+    Module:HookFriendsColor(db.first.friendsColor)
+end
+
+function Module:SetupLookupTable()
+    local localClassTable = {}
+    if GetLocale() ~= "enUS" then
+        for k, v in pairs(LOCALIZED_CLASS_NAMES_FEMALE) do
+            --
+            localClassTable[v] = k
+
+        end
+    else
+        for k, v in pairs(LOCALIZED_CLASS_NAMES_MALE) do
+            --
+            localClassTable[v] = k
+        end
+    end
+    Module.LocalClassTable = localClassTable
+end
+
+local FRIENDS_BUTTON_TYPE_DIVIDER = FRIENDS_BUTTON_TYPE_DIVIDER or 1;
+local FRIENDS_BUTTON_TYPE_BNET = FRIENDS_BUTTON_TYPE_BNET or 2;
+local FRIENDS_BUTTON_TYPE_WOW = FRIENDS_BUTTON_TYPE_WOW or 3;
+local FRIENDS_BUTTON_TYPE_INVITE = FRIENDS_BUTTON_TYPE_INVITE or 4;
+local FRIENDS_BUTTON_TYPE_INVITE_HEADER = FRIENDS_BUTTON_TYPE_INVITE_HEADER or 5;
+
+function Module:HookFriendsColor(hook)
+    self:Unhook('FriendsFrame_UpdateFriendButton')
+    if hook then
+        self:SecureHook('FriendsFrame_UpdateFriendButton', function(button)
+            if (button.buttonType == FRIENDS_BUTTON_TYPE_BNET) then
+                local index = button.index
+                local bnetIDAccount, accountName, battleTag, isBattleTag, characterName, bnetIDGameAccount, client,
+                      isOnline, lastOnline, isBnetAFK, isBnetDND, messageText, noteText, isRIDFriend, messageTime,
+                      canSoR = BNGetFriendInfo(button.id);
+
+                if not isOnline or client ~= BNET_CLIENT_WOW then return; end
+
+                local hasFocus, characterName, client, realmName, realmID, faction, race, class, guild, zoneName, level,
+                      gameText, broadcastText, broadcastTime, canSoR, toonID, bnetIDAccount, isGameAFK, isGameBusy =
+                    BNGetGameAccountInfo(bnetIDGameAccount)
+
+                -- print(accountName, characterName, class, faction)
+
+                if (characterName) then
+                    local classFixed = Module.LocalClassTable[class]
+                    local color = RAID_CLASS_COLORS[classFixed]
+
+                    if not color then return end
+
+                    local englishFaction, localizedFaction = UnitFactionGroup('player')
+
+                    nameText = accountName .. " " .. "|c" .. color.colorStr .. "(" .. characterName .. ': ' .. level ..
+                                   ")" .. FONT_COLOR_CODE_CLOSE;
+
+                    if faction ~= localizedFaction then nameText = nameText .. ' (' .. faction .. ')' end
+
+                    button.name:SetText(nameText)
+                end
+            end
+        end)
+    end
 end
 
 local frame = CreateFrame('FRAME')
