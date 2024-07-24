00f7691875708e86d8a10c61aa0d7ae4ed4ba09b
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-07-24 13:07:40 +0200

    setup

--- Core.lua
+++ Core.lua
@@ -1,4 +1,4 @@
-local DF = LibStub('AceAddon-3.0'):NewAddon('DragonflightUI', 'AceConsole-3.0', 'AceHook-3.0')
+local DF = LibStub('AceAddon-3.0'):NewAddon('DragonflightUI', 'AceConsole-3.0', 'AceComm-3.0', 'AceHook-3.0')
 local db
 
 DF.InterfaceVersion = select(4, GetBuildInfo())
@@ -15,6 +15,7 @@ function DF:OnInitialize()
     db = self.db.profile
     self:SetupOptions()
     self:RegisterSlashCommands()
+    self:InitVersionCheck()
 end
 
 function DF:OnEnable()
