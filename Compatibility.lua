5e73076c1ee2f6d4f61ae85a04a375cd06658054
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-08-24 15:10:47 +0200

    Update Compatibility.lua

--- Compatibility.lua
+++ Compatibility.lua
@@ -110,7 +110,7 @@ end
 
 function DF.Compatibility:LFGBulletinBoard(func)
     -- related issues: #235, Vysci/LFG-Bulletin-Board#301
-    local btn = _G['LFGBulletinBoardMinimapButton']
+    local btn = _G['LFGBulletinBoardMinimapButton'] or _G['Lib_GPI_Minimap_LFGBulletinBoard']
     if not btn then return end
     func(btn)
 end
