fe72f7863a58de1960827aee90313be67f891d10
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-02-20 00:27:36 +0100

    TargetFrame: Fix TargetOfTarget buff position

--- Modules/Unitframe.lua
+++ Modules/Unitframe.lua
@@ -3797,7 +3797,7 @@ function Module.ChangeToT()
     TargetFrameToTTextureFrameUnconsciousText:ClearAllPoints()
     TargetFrameToTTextureFrameUnconsciousText:SetPoint('CENTER', TargetFrameToTHealthBar, 'CENTER', 0, 0)
 
-    TargetFrameToTDebuff1:SetPoint('TOPLEFT', TargetFrameToT, 'TOPRIGHT', 25, -20)
+    TargetFrameToTDebuff1:SetPoint('TOPLEFT', TargetFrameToT, 'TOPRIGHT', 5, -20)
 end
 
 function Module.ChangeFocusFrame()
