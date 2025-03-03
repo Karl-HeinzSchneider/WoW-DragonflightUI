d70afc07db0c66cb15d68571a5ab28a3a3c50458
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-03-03 14:15:57 +0100

    r

--- Modules/Buffs.lua
+++ Modules/Buffs.lua
@@ -504,13 +504,11 @@ end
 
 function Module:CreateNewBuffs()
     local header = CreateFrame("Frame", "DragonflightUIBuffFrameHeader", nil, "SecureAuraHeaderTemplate");
-    header:SetAttribute("unit", "player"); -- to activate UNITAURA event refresh
-    header:SetAttribute("filter", "HELPFUL");
-    header:SetAttribute("template", "DragonflightUIAuraButtonTemplate"); -- must be the template name of your XML
+    header:SetAttribute("unit", "player");
+    header:SetAttribute("template", "DragonflightUIAuraButtonTemplate");
     header:SetAttribute("minWidth", 100);
     header:SetAttribute("minHeight", 100);
 
-    -- the following code create a buff bar anchor to TOPLEFT and buff orientation goes to right with 10 buttons max on two rows
     header:SetAttribute("point", "TOPRIGHT");
     header:SetAttribute("xOffset", -30 - 5);
     header:SetAttribute("yOffset", 0);
@@ -520,8 +518,12 @@ function Module:CreateNewBuffs()
     header:SetAttribute("maxWraps", 2);
 
     -- sorting
-    header:SetAttribute("sortMethod", "NAME"); -- INDEX or NAME or TIME
-    header:SetAttribute("sortDir", "+"); -- - to reverse
+    header:SetAttribute("filter", "HELPFUL");
+    header:SetAttribute("separateOwn", "0");
+
+    header:SetAttribute("sortMethod", "INDEX"); -- INDEX or NAME or TIME
+    header:SetAttribute("sortDir", "-"); -- - to reverse
+
     header:Show();
 
     header:SetPoint('TOPRIGHT', Module.DFBuffFrame, 'TOPRIGHT', 0, 0)
@@ -538,12 +540,99 @@ function Module:CreateNewBuffs()
     end
 
     -- The update style function
+    header.BuffFrameUpdateTime = 0;
+    header.BuffFrameFlashTime = 0;
+    header.BuffFrameFlashState = 1;
+    header.BuffAlphaValue = 1;
+
+    local TOOLTIP_UPDATE_TIME = TOOLTIP_UPDATE_TIME or 0.2;
+    local BUFF_FLASH_TIME_ON = BUFF_FLASH_TIME_ON or 0.75;
+    local BUFF_FLASH_TIME_OFF = BUFF_FLASH_TIME_OFF or 0.75;
+    local BUFF_MIN_ALPHA = BUFF_MIN_ALPHA or 0.3;
+
+    header:SetScript('OnUpdate', function(self, elapsed)
+        --
+        if (self.BuffFrameUpdateTime > 0) then
+            self.BuffFrameUpdateTime = self.BuffFrameUpdateTime - elapsed;
+        else
+            self.BuffFrameUpdateTime = self.BuffFrameUpdateTime + TOOLTIP_UPDATE_TIME;
+        end
+
+        self.BuffFrameFlashTime = self.BuffFrameFlashTime - elapsed;
+        if (self.BuffFrameFlashTime < 0) then
+            local overtime = -self.BuffFrameFlashTime;
+            if (self.BuffFrameFlashState == 0) then
+                self.BuffFrameFlashState = 1;
+                self.BuffFrameFlashTime = BUFF_FLASH_TIME_ON;
+            else
+                self.BuffFrameFlashState = 0;
+                self.BuffFrameFlashTime = BUFF_FLASH_TIME_OFF;
+            end
+            if (overtime < self.BuffFrameFlashTime) then
+                self.BuffFrameFlashTime = self.BuffFrameFlashTime - overtime;
+            end
+        end
+
+        if (self.BuffFrameFlashState == 1) then
+            self.BuffAlphaValue = (BUFF_FLASH_TIME_ON - self.BuffFrameFlashTime) / BUFF_FLASH_TIME_ON;
+        else
+            self.BuffAlphaValue = self.BuffFrameFlashTime / BUFF_FLASH_TIME_ON;
+        end
+        self.BuffAlphaValue = (self.BuffAlphaValue * (1 - BUFF_MIN_ALPHA)) + BUFF_MIN_ALPHA;
+    end)
+
+    local BUFF_DURATION_WARNING_TIME = BUFF_DURATION_WARNING_TIME or 60;
+    local BUFF_WARNING_TIME = BUFF_WARNING_TIME or 31;
+
+    local function updateAuraDuration(btn, elapsed)
+        --
+        -- print('updateAuraDuration', btn:GetName(), elapsed)
+        local name, icon, count, dispelType, duration, expirationTime, source, isStealable, nameplateShowPersonal,
+              spellId, canApplyAura, isBossDebuff, castByPlayer, nameplateShowAll, timeMod = UnitAura("player",
+                                                                                                      btn:GetID(),
+                                                                                                      "HELPFUL");
+        -- print(timeMod)
+        if name and duration > 0 then
+            btn.Duration:Show()
+
+            local timeLeft = (expirationTime - GetTime());
+            if (timeMod > 0) then
+                --
+                timeLeft = timeLeft / timeMod;
+            end
+
+            timeLeft = max(timeLeft, 0);
+
+            btn.Duration:SetFormattedText(SecondsToTimeAbbrev(timeLeft));
+            if (timeLeft < BUFF_DURATION_WARNING_TIME) then
+                btn.Duration:SetVertexColor(HIGHLIGHT_FONT_COLOR.r, HIGHLIGHT_FONT_COLOR.g, HIGHLIGHT_FONT_COLOR.b);
+            else
+                btn.Duration:SetVertexColor(NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b);
+            end
+
+            if (timeLeft < BUFF_WARNING_TIME) then
+                btn:SetAlpha(header.BuffAlphaValue);
+            else
+                btn:SetAlpha(1.0);
+            end
+
+            if (header.BuffFrameUpdateTime > 0) then return; end
+            if (GameTooltip:IsOwned(btn)) then
+                GameTooltip:SetUnitAura(PlayerFrame.unit, btn:GetID(), 'HELPFUL');
+            end
+        else
+            btn.Duration:Hide()
+            btn:SetAlpha(1.0);
+        end
+    end
+
     local function updateStyle()
         for _, frame in header:ActiveChildren() do
 
             if not frame.Cooldown then
-                local cd = CreateFrame("Cooldown", nil, frame)
+                local cd = CreateFrame("Cooldown", nil, frame, 'CooldownFrameTemplate')
                 cd:SetAllPoints(frame.Icon)
+                cd:SetSwipeTexture('Interface\\Addons\\DragonflightUI\\Textures\\maskNewAlpha', 1.0, 1.0, 1.0, 0.8)
                 cd:SetReverse(true)
                 cd.noCooldownCount = true -- no OmniCC timers
                 cd:SetFrameLevel(3)
@@ -554,51 +643,53 @@ function Module:CreateNewBuffs()
                   spellId, canApplyAura, isBossDebuff, castByPlayer, nameplateShowAll, timeMod = UnitAura("player",
                                                                                                           frame:GetID(),
                                                                                                           "HELPFUL");
-            print('~', name, icon, count, expirationTime - duration)
+            -- print('~', name, icon, count, expirationTime - duration)
             if name then
                 frame.Icon:SetTexture(icon);
                 frame.Icon:Show();
 
                 if count > 1 then
-                    frame.count:SetText(count);
+                    frame.Count:SetText(count);
+                    frame.count:Show();
                 else
-                    frame.count:SetText("");
+                    frame.Count:SetText("");
                 end
 
                 if duration > 0 then
                     frame.Cooldown:SetCooldown(expirationTime - duration, duration);
                     frame.Cooldown:SetAlpha(1.0);
                     frame.Cooldown:Show();
-                    print('dur', expirationTime - duration, duration)
+                    -- print('dur', expirationTime - duration, duration)
+                    frame.Duration:Show()
+                    frame.Duration:SetText(duration)
+
+                    frame:SetScript('OnUpdate', updateAuraDuration)
+                    updateAuraDuration(frame, 0);
                 else
                     frame.Cooldown:Hide();
                     frame.Cooldown:SetCooldown(0, -1);
                     frame.Cooldown:SetAlpha(0);
+                    frame.Duration:Hide()
+                    frame:SetScript('OnUpdate', nil)
+                    frame:SetAlpha(1.0)
                 end
-
-                frame.count:Show();
             else
                 frame.Icon:Hide();
-                frame.count:Hide();
+                frame.Count:Hide();
                 frame.Cooldown:Hide();
                 frame.Cooldown:SetCooldown(0, -1);
                 frame.Cooldown:SetAlpha(0);
-
+                frame:SetScript('OnUpdate', nil)
             end
         end
     end
 
-    -- header:RegisterEvent("UNIT_AURA");
-    -- header:SetScript('OnEvent', function(event, ...)
-    --     --
-    --     print('OnEvent:', event, ...)
-    --     updateStyle()
-    -- end)
     header:HookScript('OnEvent', function(event, ...)
         --
-        print('OnEvent:', event, ...)
+        -- print('OnEvent:', event, ...)
         updateStyle()
     end)
+    updateStyle()
 end
 
 -- set default, may be overriden by darkmode module
