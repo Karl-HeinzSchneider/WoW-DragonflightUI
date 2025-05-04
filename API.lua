d7bb9ed46c1b493d45b24cb760b73a8798ac160b
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-05-04 04:25:15 +0200

    Update API.lua

--- API.lua
+++ API.lua
@@ -16,6 +16,10 @@ DF.EraLater = DF.Era and DF.InterfaceVersion >= 11503
 local Version = {}
 API.Version = Version
 
+--- WoW Interface Version, e.g. 11507
+---@type number
+Version.InterfaceVersion = select(4, GetBuildInfo())
+
 --- Addon is running on Classic "Vanilla" client, e.g. Era and SoD etc
 ---@type boolean
 Version.IsClassic = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
@@ -26,8 +30,9 @@ Version.IsSoD = Version.IsClassic and C_Seasons.HasActiveSeason() and
                     (C_Seasons.GetActiveSeason() == Enum.SeasonID.SeasonOfDiscovery)
 
 --- current Season ID, or 0 if no Season
+--- 
 --- https://warcraft.wiki.gg/wiki/API_C_Seasons.GetActiveSeason
----@type number
+---@type Enum.SeasonID
 Version.SeasonID = 0;
 if C_Seasons.HasActiveSeason() then Version.SeasonID = C_Seasons.GetActiveSeason() end
 
@@ -47,4 +52,4 @@ Version.IsCata = WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC
 ---@type boolean
 Version.IsMoP = WOW_PROJECT_ID == WOW_PROJECT_MISTS_CLASSIC
 
-DevTools_Dump(API.Version)
+-- DevTools_Dump(API.Version)
