2de9048fe5b1222c48559d53f33ec028905dc28a
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-05-05 03:51:51 +0200

    skin slot

--- Compatibility/Baganator.lua
+++ Compatibility/Baganator.lua
@@ -13,6 +13,39 @@ function DF.Compatibility:Baganator()
 
     local skinners = {
         ItemButton = function(frame)
+            -- frame.SlotBackground:SetAlpha(0.5)
+            local bg = frame.SlotBackground
+            bg:SetTexture(base .. 'BagsItemSlot2x')
+            bg:SetSize(37, 37)
+            bg:SetPoint('CENTER', 0, 0)
+            bg:SetDrawLayer('BACKGROUND', 3)
+
+            local normal = frame:GetNormalTexture()
+            normal:SetTexture(base .. 'BagsItemSlot2x')
+            normal:SetSize(37, 37)
+            normal:SetPoint('CENTER', 0, 0)
+            normal:SetDrawLayer('BACKGROUND', 3)
+
+            local pushed = frame:GetPushedTexture()
+            pushed:SetTexture(base .. 'ui-quickslot-depress')
+            pushed:SetSize(37, 37)
+            pushed:SetPoint('CENTER', 0, 0)
+
+            local high = frame:GetHighlightTexture()
+            high:SetTexture(base .. 'buttonhilight-square')
+            -- high:SetTexCoord(0.408203, 0.478516, 0.679688, 0.820312)
+            high:SetSize(37, 37)
+            high:SetPoint('CENTER', 0, 0)
+            -- high:SetDrawLayer('BACKGROUND', 3)
+
+            -- local iconBorder = frame.IconBorder
+            -- iconBorder:Hide()
+
+            local border = frame:CreateTexture('DragonflightUIBorder')
+            border:SetTexture(base .. 'ui-quickslot2')
+            border:SetSize(64, 64)
+            border:SetPoint('CENTER', 0, -1)
+            border:SetDrawLayer('BACKGROUND', 4)
         end,
         IconButton = function(button, tags)
             if tags.sort then
