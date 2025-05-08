4e2fa638e30cac75dbcf8f20cf2157ede8c03458
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-05-09 00:27:58 +0200

    Init module

---
+++ Modules/Compatibility.lua
@@ -0,0 +1,108 @@
+local DF = LibStub('AceAddon-3.0'):GetAddon('DragonflightUI')
+local L = LibStub("AceLocale-3.0"):GetLocale("DragonflightUI")
+local mName = 'Compatibility'
+local Module = DF:NewModule(mName, 'AceConsole-3.0', 'AceHook-3.0')
+
+Mixin(Module, DragonflightUIModulesMixin)
+
+local defaults = {profile = {scale = 1, general = {}}}
+Module:SetDefaults(defaults)
+
+local function getDefaultStr(key, sub, extra)
+    return Module:GetDefaultStr(key, sub, extra)
+end
+
+local function setDefaultValues()
+    Module:SetDefaultValues()
+end
+
+local function setDefaultSubValues(sub)
+    Module:SetDefaultSubValues(sub)
+end
+
+local function getOption(info)
+    return Module:GetOption(info)
+end
+
+local function setOption(info, value)
+    Module:SetOption(info, value)
+end
+
+function Module:OnInitialize()
+    DF:Debug(self, 'Module ' .. mName .. ' OnInitialize()')
+    self.db = DF.db:RegisterNamespace(mName, defaults)
+    hooksecurefunc(DF:GetModule('Config'), 'AddConfigFrame', function()
+        Module:RegisterSettings()
+    end)
+
+    self:SetEnabledState(DF.ConfigModule:GetModuleEnabled(mName))
+
+    -- DF:RegisterModuleOptions(mName, generalOptions)
+end
+
+function Module:OnEnable()
+    DF:Debug(self, 'Module ' .. mName .. ' OnEnable()')
+    self:SetWasEnabled(true)
+
+    -- if DF.Cata then
+    --     Module.Cata()
+    -- elseif DF.Wrath then
+    --     Module.Wrath()
+    -- else
+    --     Module.Era()
+    -- end
+
+    -- Module:AddEditMode()
+
+    Module:ApplySettings()
+    Module:RegisterOptionScreens()
+
+    self:SecureHook(DF, 'RefreshConfig', function()
+        -- print('RefreshConfig', mName)
+        Module:ApplySettings()
+        Module:RefreshOptionScreens()
+    end)
+end
+
+function Module:OnDisable()
+end
+
+function Module:RegisterSettings()
+    local moduleName = 'Compatibility'
+    local cat = 'general'
+    local function register(name, data)
+        data.module = moduleName;
+        DF.ConfigModule:RegisterSettingsElement(name, cat, data, true)
+    end
+
+    register('compatibility', {order = 0, name = 'Compatibility', descr = '...', isNew = true})
+end
+
+function Module:RegisterOptionScreens()
+    -- DF.ConfigModule:RegisterSettingsData('tooltip', 'misc', {
+    --     name = 'Tooltip',
+    --     sub = 'general',
+    --     options = generalOptions,
+    --     sortComparator = generalOptions.sortComparator,
+    --     default = function()
+    --         setDefaultSubValues('general')
+    --     end
+    -- })
+end
+
+function Module:RefreshOptionScreens()
+    -- print('Module:RefreshOptionScreens()')
+
+    -- local configFrame = DF.ConfigModule.ConfigFrame
+    -- local cat = 'Misc'
+    -- configFrame:RefreshCatSub(cat, 'Tooltip')
+
+    -- Module.GametooltipPreview.DFEditModeSelection:RefreshOptionScreen();
+end
+
+function Module:ApplySettings(sub)
+    local db = Module.db.profile
+    local state = db.general
+
+end
+
