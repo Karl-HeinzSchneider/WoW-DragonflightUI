229fe25873f02423b88e8b51b3b756266477844e
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-05-23 18:19:14 +0200

    s

--- Mixin/Spellflyout.mixin.lua
+++ Mixin/Spellflyout.mixin.lua
@@ -57,11 +57,56 @@ function DragonflightUISpellFlyoutMixin:AddWarlockButtons(t)
     print('DragonflightUISpellFlyoutMixin:AddWarlockButtons()', #t)
 
     local sum = t[1]
+    local sumButtons = {}
     sum.Icon:SetTexture(136082)
+
+    local spells = {688, 697, 712, 691, 1122}
+
+    for k, v in ipairs(spells) do
+        local btn = self:CreateSpellButton(v, sum:GetName() .. 'Spell' .. k, sum)
+
+        sumButtons[k] = btn;
+
+        if k == 1 then
+            btn:SetPoint("BOTTOM", sum, "TOP", 0, 4)
+        else
+            btn:SetPoint("BOTTOM", sumButtons[k - 1], "TOP", 0, 4)
+        end
+    end
 end
--- function DragonflightUISpellFlyoutMixin:OnLoad()
---     print('DragonflightUISpellFlyoutMixin:OnLoad()')
--- end
+
+function DragonflightUISpellFlyoutMixin:CreateSpellButton(spell, id, parent)
+    print('DragonflightUISpellFlyoutMixin:CreateSpellButton()', id)
+    local btn = CreateFrame("CheckButton", id, parent, "DFSpellFlyoutSubButtonTemplate")
+    self:StyleButton(btn)
+    btn:SetScale(0.9)
+
+    local name, rank, icon, castTime, minRange, maxRange = GetSpellInfo(spell)
+    local learned = IsPlayerSpell(spell)
+    print(name, learned)
+    -- btn:SetAttribute("type", "action")
+    -- btn:SetAttribute("action", 144 + (n - 6) * 12 + i) -- Action slot 1
+    btn:SetAttribute("type", "spell");
+    btn:SetAttribute("spell", name);
+    _G[btn:GetName() .. 'Icon']:SetTexture(icon)
+    btn:SetFrameLevel(3)
+
+    btn:SetScript('OnEnter', function()
+        GameTooltip:SetOwner(btn, "ANCHOR_RIGHT");
+        GameTooltip:SetSpellByID(spell)
+
+        if IsPlayerSpell(spell) then
+            print('true')
+        else
+            print('false')
+        end
+    end)
+    btn:SetScript('OnLeave', function()
+        GameTooltip:Hide()
+    end)
+
+    return btn;
+end
 -- function DragonflightUISpellFlyoutMixin:OnLoad()
 --     print('DragonflightUISpellFlyoutMixin:OnLoad()')
 -- end
