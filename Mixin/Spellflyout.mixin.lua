18d209c8a38be138dcd128f3ba16619a4f62456c
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-05-27 04:08:47 +0200

    f

--- Mixin/Spellflyout.mixin.lua
+++ Mixin/Spellflyout.mixin.lua
@@ -1,6 +1,8 @@
 local DF = LibStub('AceAddon-3.0'):GetAddon('DragonflightUI')
 local L = LibStub("AceLocale-3.0"):GetLocale("DragonflightUI")
 
+local GetItemCooldown = GetItemCooldown or C_Container.GetItemCooldown;
+
 local textureRef = 'Interface\\Addons\\DragonflightUI\\Textures\\uiactionbar'
 local textureRefTwo = 'Interface\\Addons\\DragonflightUI\\Textures\\uiactionbar2x'
 
@@ -125,6 +127,8 @@ function DragonflightUISpellFlyoutButtonMixin:InitButtons()
         btn:SetFrameLevel(3)
         btn.Icon:SetTexture(136082)
 
+        btn.Count = _G[btn:GetName() .. 'Count']
+        btn.Count:SetText('69')
         -- if i == 1 then
         --     btn:SetPoint("BOTTOM", self, "TOP", 0, 4)
         -- else
@@ -174,50 +178,88 @@ function DragonflightUISpellFlyoutButtonMixin:Update()
         -- print('hide', i)
     end
 
-    local numUsed = 0;
+    local macroTable = {}
+
+    -- all spells
     if state.spells ~= '' then
         local spellTable = self:SplitString(state.spells)
-        -- print('spells: ', state.spells)
-        -- DevTools_Dump(spellTable)
-        numUsed = math.min(state.buttons, #spellTable)
-        self.BG:SetState(state, numUsed)
 
-        for i = 1, numUsed do
-            --
-            local btn = buttonTable[i]
-            -- btn:ClearAllPoints()
-            -- btn:Show()
-            self.BG:SetButton(btn, i, buttonTable)
-
-            local spell = spellTable[i];
-
+        for k, spell in ipairs(spellTable) do
+            --        
             local name, rank, icon, castTime, minRange, maxRange = GetSpellInfo(spell)
-            local learned = IsPlayerSpell(spell)
-            -- print(spell, name, learned)
+            if name then table.insert(macroTable, spell) end
+        end
+    end
 
-            -- btn:SetAttribute("type", "spell");
-            -- btn:SetAttribute("spell", name);
+    local englishFaction, localizedFaction = UnitFactionGroup('player')
+
+    if state.spellsAlliance ~= '' and englishFaction == 'Alliance' then
+        local spellTable = self:SplitString(state.spellsAlliance)
+
+        for k, spell in ipairs(spellTable) do
+            --        
+            local name, rank, icon, castTime, minRange, maxRange = GetSpellInfo(spell)
+            if name then table.insert(macroTable, spell) end
+        end
+    end
+
+    if state.spellsHorde ~= '' and englishFaction == 'Horde' then
+        local spellTable = self:SplitString(state.spellsHorde)
+
+        for k, spell in ipairs(spellTable) do
+            --        
+            local name, rank, icon, castTime, minRange, maxRange = GetSpellInfo(spell)
+            if name then table.insert(macroTable, spell) end
+        end
+    end
+
+    local numSpells = #macroTable;
+
+    if state.items ~= '' then
+        local spellTable = self:SplitString(state.items)
+
+        for k, spell in ipairs(spellTable) do
+            --        
+            local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,
+                  itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(spell)
+            if itemName then table.insert(macroTable, spell) end
+        end
+    end
+
+    -- DevTools_Dump(macroTable)
+    -- print('numspells', numSpells)
+    -- print('all', #macroTable)
+
+    local closeStr = ''
+    if state.closeAfterClick then closeStr = "\n/click " .. self:GetName() .. "ClickButton"; end
+
+    local numUsed = math.min(state.buttons, #macroTable);
+    self.BG:SetState(state, numUsed)
+
+    for i = 1, numUsed do
+        --
+        local btn = buttonTable[i]
+        self.BG:SetButton(btn, i, buttonTable)
+
+        if i <= numSpells then
+            local spell = macroTable[i];
+            local name, rank, icon, castTime, minRange, maxRange = GetSpellInfo(spell)
+            -- local learned = IsPlayerSpell(spell)
 
             btn:SetAttribute("type", "macro");
-            btn:SetAttribute("macrotext", "/cast " .. name .. "\n/click " .. self:GetName() .. "ClickButton");
+            btn:SetAttribute("macrotext", "#showtooltip\n/cast " .. name .. closeStr);
 
-            btn.Icon:SetTexture(icon)
+            btn:SetSpell(spell)
+        else
+            local item = macroTable[i]
+            local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,
+                  itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(item)
 
-            btn:SetScript('OnEnter', function()
-                GameTooltip:SetOwner(btn, "ANCHOR_RIGHT");
-                GameTooltip:SetSpellByID(spell)
+            btn:SetAttribute("type", "macro");
+            btn:SetAttribute("macrotext", "/use " .. itemName .. closeStr);
 
-                if IsPlayerSpell(spell) then
-                    -- print('true')
-                else
-                    -- print('false')
-                end
-            end)
-            btn:SetScript('OnLeave', function()
-                GameTooltip:Hide()
-            end)
+            btn:SetItem(item)
         end
-    else
     end
 
     for i = numUsed + 1, btnCount do
@@ -388,4 +430,233 @@ function DragonflightUISpellFlyoutBGMixin:SetButton(btn, i, buttonTable)
             btn:SetPoint("BOTTOM", buttonTable[i - 1], "TOP", 0, padding)
         end
     end
+end
+
+DragonflightUISpellSubButtonMixin = {}
+function DragonflightUISpellSubButtonMixin:OnLoad()
+    -- print('DragonflightUISpellSubButtonMixin:OnLoad()')
+
+    -- self:RegisterEvent("ACTIONBAR_UPDATE_STATE"); -- not updating state from lua anymore, see SetActionUIButton
+    self:RegisterEvent("ACTIONBAR_UPDATE_USABLE");
+    self:RegisterEvent("ACTIONBAR_UPDATE_COOLDOWN"); -- not updating cooldown from lua anymore, see SetActionUIButton
+    self:RegisterEvent("SPELL_UPDATE_CHARGES");
+    self:RegisterEvent("UPDATE_INVENTORY_ALERTS");
+    self:RegisterEvent("PLAYER_TARGET_CHANGED");
+    self:RegisterEvent("TRADE_SKILL_SHOW");
+    self:RegisterEvent("TRADE_SKILL_CLOSE");
+    self:RegisterEvent("PLAYER_ENTER_COMBAT");
+    self:RegisterEvent("PLAYER_LEAVE_COMBAT");
+    self:RegisterEvent("START_AUTOREPEAT_SPELL");
+    self:RegisterEvent("STOP_AUTOREPEAT_SPELL");
+    self:RegisterEvent("UNIT_INVENTORY_CHANGED");
+    self:RegisterEvent("LEARNED_SPELL_IN_TAB");
+    self:RegisterEvent("PET_STABLE_UPDATE");
+    self:RegisterEvent("PET_STABLE_SHOW");
+    self:RegisterUnitEvent("LOSS_OF_CONTROL_ADDED", "player");
+    self:RegisterUnitEvent("LOSS_OF_CONTROL_UPDATE", "player");
+    self:RegisterEvent("SPELL_UPDATE_ICON");
+    self:RegisterEvent("SPELL_ACTIVATION_OVERLAY_GLOW_SHOW");
+    self:RegisterEvent("SPELL_ACTIVATION_OVERLAY_GLOW_HIDE");
+
+    -- self:RegisterEvent("SPELL_UPDATE_COOLDOWN");
+end
+
+function DragonflightUISpellSubButtonMixin:OnEvent(event, ...)
+    -- if self:GetID() == 1 then print(event, ...) end
+    -- print(event, ...)
+    self:UpdateState()
+end
+
+function DragonflightUISpellSubButtonMixin:SetSpell(spell)
+    self.Spell = spell;
+    self.Item = nil;
+    self.ItemLink = nil;
+
+    local name, rank, icon, castTime, minRange, maxRange = GetSpellInfo(spell)
+    local learned = IsPlayerSpell(spell)
+
+    self.Icon:SetTexture(icon)
+
+    self:UpdateState()
+end
+
+function DragonflightUISpellSubButtonMixin:SetItem(item)
+    self.Spell = nil;
+    self.Item = item;
+
+    local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc,
+          itemTexture, itemSellPrice = GetItemInfo(item)
+    self.ItemLink = itemLink;
+
+    self.Icon:SetTexture(itemTexture)
+
+    self:UpdateState()
+end
+
+function DragonflightUISpellSubButtonMixin:UpdateState()
+    if self.Spell then
+        self:UpdateStateSpell()
+    elseif self.Item then
+        self:UpdateStateItem()
+    end
+end
+
+function DragonflightUISpellSubButtonMixin:UpdateStateSpell()
+    self.Count:SetText("");
+
+    -- local name, rank, icon, castTime, minRange, maxRange = GetSpellInfo(spell)
+    local learned = IsPlayerSpell(self.Spell)
+
+    if learned then
+        self.Icon:SetDesaturated(false);
+        local powerCosts = C_Spell.GetSpellPowerCost(self.Spell)
+        powerCosts = powerCosts[1]
+        self.Icon:SetVertexColor(0.4, 0.4, 0.4)
+        self.Icon:SetVertexColor(0.5, 0.5, 1.0)
+
+        local hasRequiredAura = true;
+        if powerCosts.requiredAuraID and powerCosts.requiredAuraID ~= 0 then
+            hasRequiredAura = powerCosts.hasRequiredAura;
+        end
+
+        local power = UnitPower("player", Enum.PowerType[powerCosts.type]);
+        local hasCost = power >= powerCosts.minCost;
+
+        -- print(self.Spell, hasRequiredAura, hasCost)
+
+        if hasRequiredAura then
+            if hasCost then
+                -- "usable"
+                self.Icon:SetVertexColor(1.0, 1.0, 1.0)
+            else
+                -- "notEnoughMana"
+                self.Icon:SetVertexColor(0.5, 0.5, 1.0)
+            end
+        else
+            self.Icon:SetVertexColor(0.4, 0.4, 0.4)
+        end
+    else
+        self.Icon:SetDesaturated(true);
+        self.Icon:SetVertexColor(1.0, 1.0, 1.0)
+    end
+
+    self:UpdateCooldown()
+end
+
+function DragonflightUISpellSubButtonMixin:UpdateStateItem()
+    local count = GetItemCount(self.Item, false, true)
+    if (count > (self.maxDisplayCount or 9999)) then
+        self.Count:SetText("*");
+    else
+        self.Count:SetText(count)
+    end
+
+    if count > 0 then
+        self.Icon:SetVertexColor(1.0, 1.0, 1.0)
+    else
+        self.Icon:SetVertexColor(0.4, 0.4, 0.4)
+    end
+
+    self:UpdateCooldown()
+end
+
+local bandageIDs = {
+    8545, 6450, 6451, 3531, 8544, 2581, 3530, 1251, 20244, 20235, 19068, 20067, 20232, 20065, 19067, 20237, 14529,
+    19066, 20234, 20243, 20066, 14530, 19307, 23684
+}
+local isBandageID = {}
+for k, v in ipairs(bandageIDs) do
+    --
+    isBandageID[v] = true;
+end
+
+function DragonflightUISpellSubButtonMixin:UpdateCooldown()
+    local start, duration, enable -- , charges, maxCharges, chargeStart, chargeDuration;
+    local modRate = 1.0;
+    -- local chargeModRate = 1.0;
+
+    if self.Spell then
+        start, duration, enable, modRate = GetSpellCooldown(self.Spell);
+    elseif self.Item then
+        -- start, duration, enable, modRate = GetActionCooldown(self.action);
+        -- charges, maxCharges, chargeStart, chargeDuration, chargeModRate = GetActionCharges(self.action);
+        if isBandageID[self.Item] then
+            -- print('bandage')
+            local auraData = self:GetBandage()
+
+            if auraData then
+                start = auraData.expirationTime - auraData.duration;
+                duration = auraData.duration;
+                enable = true;
+            end
+        else
+            start, duration, enable = GetItemCooldown(self.Item)
+        end
+    end
+
+    if (self.cooldown.currentCooldownType ~= COOLDOWN_TYPE_NORMAL) then
+        self.cooldown:SetEdgeTexture("Interface\\Cooldown\\edge");
+        self.cooldown:SetSwipeColor(0, 0, 0);
+        self.cooldown:SetHideCountdownNumbers(false);
+        self.cooldown.currentCooldownType = COOLDOWN_TYPE_NORMAL;
+    end
+
+    -- if (charges and maxCharges and maxCharges > 1 and charges < maxCharges) then
+    --     StartChargeCooldown(self, chargeStart, chargeDuration, chargeModRate);
+    -- else
+    --     ClearChargeCooldown(self);
+    -- end
+
+    -- if enable then
+    --     self.Icon:SetVertexColor(0.4, 0.4, 0.4)
+    -- else
+    --     self.Icon:SetVertexColor(1.0, 1.0, 1.0)
+    -- end
+
+    CooldownFrame_Set(self.cooldown, start, duration, enable, false, modRate);
+end
+
+function DragonflightUISpellSubButtonMixin:GetBandage()
+    local bandageDebuffName = GetSpellInfo(11196);
+
+    local i = 1;
+    repeat
+        --
+        local aura = {}
+
+        local auraData = C_UnitAuras.GetAuraDataByIndex('player', i, 'HARMFUL');
+        if (auraData == nil) then
+            aura.name = nil;
+        else
+            aura.name = auraData.name;
+            aura.duration = auraData.duration;
+            aura.expires = auraData.expirationTime;
+            aura.caster = auraData.sourceUnit;
+            aura.shouldConsolidate = #auraData.points > 0;
+        end
+
+        if aura.name == bandageDebuffName then return auraData; end
+
+        -- print(i, aura.name, aura.duration, aura.expires)
+
+        i = i + 1;
+    until (not aura.name)
+
+    return nil;
+end
+
+function DragonflightUISpellSubButtonMixin:OnEnter()
+    if self.Spell then
+        GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
+        GameTooltip:SetSpellByID(self.Spell)
+        GameTooltip:Show()
+    elseif self.Item and self.ItemLink then
+        GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
+        GameTooltip:SetHyperlink(self.ItemLink)
+        GameTooltip:Show()
+    end
+end
+
+function DragonflightUISpellSubButtonMixin:OnLeave()
+    GameTooltip:Hide()
 end
