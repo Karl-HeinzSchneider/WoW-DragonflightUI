0bd73cccc1eb332d42db5d906223a9870ce7ef45
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-06-09 03:22:05 +0200

    Update enUS.lua

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -392,7 +392,7 @@ L["FlyoutMageFoodDesc"] = L["FlyoutMageWaterDesc"]
 
 L["FlyoutButtonCustomFormat"] = "Custom Flyout %d"
 L["FlyoutCustomNameFormat"] = "Custom Flyout %d"
-L["FlyoutCustomNameDescFormat"] = "ss"
+L["FlyoutCustomNameDescFormat"] = "Per-character flyout button with up to 12 additional actionbuttons."
 
 -- Castbar
 L["CastbarName"] = "Cast Bar"
