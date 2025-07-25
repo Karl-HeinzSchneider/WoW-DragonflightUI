05b9bf3e3a6bc0ea54cd257a2b6df6264ee92e41
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-07-26 00:30:06 +0200

    Update AltPower.mixin.lua

--- Modules/Unitframe/AltPower.mixin.lua
+++ Modules/Unitframe/AltPower.mixin.lua
@@ -61,6 +61,12 @@ function SubModuleMixin:SetupOptions()
         Module:RefreshOptionScreens()
     end
 
+    local frameTable = {
+        {value = 'UIParent', text = 'UIParent', tooltip = 'descr', label = 'label'},
+        {value = 'PlayerFrame', text = 'PlayerFrame', tooltip = 'descr', label = 'label'},
+        {value = 'TargetFrame', text = 'TargetFrame', tooltip = 'descr', label = 'label'}
+    }
+
     local optionsAltPower = {
         name = L["PowerBarAltName"],
         desc = L["PowerBarAltNameDesc"],
@@ -86,7 +92,7 @@ function SubModuleMixin:SetupOptions()
                 desc = L["ExtraOptionsPresetDesc"],
                 func = function()
                     local dbTable = Module.db.profile.altpower
-                    local defaultsTable = defaults.profile.altpower
+                    local defaultsTable = self.Defaults
                     -- {scale = 1.0, anchor = 'TOPLEFT', anchorParent = 'TOPLEFT', x = -19, y = -4}
                     setPreset(dbTable, {
                         scale = defaultsTable.scale,
@@ -110,15 +116,43 @@ end
 
 function SubModuleMixin:Setup()
     local function setDefaultSubValues(sub)
-        Module:SetDefaultSubValues(sub)
+        self.ModuleRef:SetDefaultSubValues(sub)
     end
 
-    DF.ConfigModule:RegisterSettingsData('player', 'unitframes', {
+    DF.ConfigModule:RegisterSettingsData('altpower', 'unitframes', {
         options = self.Options,
         default = function()
-            setDefaultSubValues('player')
+            setDefaultSubValues('altpower')
         end
     })
+
+    --
+    self:AddPowerBarAlt()
+
+    -- edit mode
+    local EditModeModule = DF:GetModule('Editmode');
+    local f = self.PowerBarAltPreview
+    EditModeModule:AddEditModeToFrame(f)
+
+    f.DFEditModeSelection:SetGetLabelTextFunction(function()
+        return self.Options.name
+    end)
+
+    f.DFEditModeSelection:RegisterOptions({
+        options = self.Options,
+        extra = self.OptionsEditmode,
+        default = function()
+            setDefaultSubValues('altpower')
+        end,
+        moduleRef = self.ModuleRef,
+        showFunction = function()
+            self:UpdatePowerBarAltPosition()
+        end,
+        hideFunction = function()
+            --
+            f:Show()
+        end
+    });
 end
 
 function SubModuleMixin:OnEvent(event, ...)
@@ -127,9 +161,62 @@ end
 function SubModuleMixin:UpdateState(state)
     self.state = state;
     self:Update();
+
+    local f = self.PowerBarAltPreview
+
+    local parent;
+    if DF.Settings.ValidateFrame(state.customAnchorFrame) then
+        parent = _G[state.customAnchorFrame]
+    else
+        parent = _G[state.anchorFrame]
+    end
+
+    f:SetScale(state.scale)
+    f:ClearAllPoints()
+    f:SetPoint(state.anchor, parent, state.anchorParent, state.x, state.y)
+
+    self:UpdatePowerBarAltPosition();
 end
 
 function SubModuleMixin:Update()
     local state = self.state;
     if not state then return end
 end
+
+function SubModuleMixin:AddPowerBarAlt()
+    local f = CreateFrame('FRAME', 'DragonflightUIPlayerPowerBarAlt', UIParent)
+    f:SetPoint('CENTER', UIParent, 'CENTER', 0, -180)
+    f:SetSize(130, 75)
+    f:SetClampedToScreen(true)
+
+    self.PowerBarAltPreview = f
+
+    local alt = _G['PlayerPowerBarAlt']
+    alt:SetMovable(true)
+    alt:SetUserPlaced(true)
+
+    function self:UpdatePowerBarAltPosition()
+        -- local state = Module.db.profile.altpower
+        -- alt:ClearAllPoints()
+        -- alt:SetPoint(state.anchor, parent, state.anchorParent, state.x, state.y)
+        -- alt:SetScale(state.scale)
+        alt:ClearAllPoints()
+        alt:SetParent(f)
+        alt:SetPoint('CENTER', f, 'CENTER', 0, 0)
+    end
+
+    hooksecurefunc('UnitPowerBarAlt_SetUp', function(bar, barID)
+        --
+        -- print('UnitPowerBarAlt_SetUp')
+        if bar.unit and UnitIsUnit(bar.unit, 'player') then
+            -- print('~~player')
+            self:UpdatePowerBarAltPosition();
+        end
+    end)
+
+    hooksecurefunc('PlayerBuffTimerManager_UpdateTimers', function(bar)
+        -- print('PlayerBuffTimerManager_UpdateTimers')
+        self:UpdatePowerBarAltPosition();
+    end)
+end
+
