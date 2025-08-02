c3845a03938554a614d287b646861ca46f2ab160
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-08-02 21:58:54 +0200

    Improve benchmark

--- Helper.lua
+++ Helper.lua
@@ -6,8 +6,13 @@ local Helper = {};
 addonTable.Helper = Helper;
 
 function Helper:Benchmark(label, func, level)
-    level = level or 1;
-    if level < 1 then DF:Debug(DF, string.format('|cffffd100-----Start Bench: |r|cff8080ff%s|r-----', label)) end
+    if level == nil or type(level) ~= 'number' then level = 1; end
+    -- level = level or 1;
+    if level < 1 then
+        local firstStr = string.format('|cffffd100-----Start Bench: |r|cff8080ff%s|r-----', label)
+        print(firstStr)
+        DF:Debug(DF, firstStr)
+    end
     local startTime = GetTimePreciseSec()
     local results = {func()}
     local endTime = GetTimePreciseSec()
@@ -20,6 +25,7 @@ function Helper:Benchmark(label, func, level)
     --                           duration * 1000, duration)
     local str = string.format("|cffffd100%sBench: |r|cff8080ff%s|r took |cffffd100%.4f|r ms", levelStr, label,
                               duration * 1000)
-    -- print(str)
+    print(str)
     DF:Debug(DF, str)
+    return results, duration, startTime, endTime;
 end
