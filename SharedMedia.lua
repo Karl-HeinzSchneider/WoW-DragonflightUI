1df2c03d5c8967942a0f7dc8f64fce98a939b07e
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-08-12 02:17:36 +0200

    Update SharedMedia.lua

--- SharedMedia.lua
+++ SharedMedia.lua
@@ -13,31 +13,32 @@ local function RegisterStatusbar(n, tex)
     LSM:Register('statusbar', pre .. n, texDF .. tex)
 end
 
--- player
-RegisterStatusbar('Player Health', [[Unitframe\UI-HUD-UnitFrame-Player-PortraitOn-Bar-Health]])
-RegisterStatusbar('Player Health Status', [[Unitframe\UI-HUD-UnitFrame-Player-PortraitOn-Bar-Health-Status]])
-RegisterStatusbar('Player Health Mask', [[Unitframe2x\uiunitframeplayerhealthmask2x]])
-RegisterStatusbar('Player Health Mask2', [[Unitframe2x\plunderstormplayerhealthbarmask2x]])
-RegisterStatusbar('Player Mana Mask', [[Unitframe2x\uiunitframeplayermanamask2x]])
+function Helper:RegisterSharedMediaStatusbars()
+    -- player
+    RegisterStatusbar('Player Health', [[Unitframe\UI-HUD-UnitFrame-Player-PortraitOn-Bar-Health]])
+    RegisterStatusbar('Player Health Status', [[Unitframe\UI-HUD-UnitFrame-Player-PortraitOn-Bar-Health-Status]])
+    RegisterStatusbar('Player Health Mask', [[Unitframe2x\uiunitframeplayerhealthmask2x]])
+    RegisterStatusbar('Player Health Mask2', [[Unitframe2x\plunderstormplayerhealthbarmask2x]])
+    RegisterStatusbar('Player Mana Mask', [[Unitframe2x\uiunitframeplayermanamask2x]])
 
-local energyTypes = {'Mana', 'Rage', 'Focus', 'Energy', 'RunicPower'}
-for i, n in ipairs(energyTypes) do
-    --
-    RegisterStatusbar('Player Bar ' .. n, [[Unitframe\UI-HUD-UnitFrame-Player-PortraitOn-Bar-]] .. n)
+    local energyTypes = {'Mana', 'Rage', 'Focus', 'Energy', 'RunicPower'}
+    for i, n in ipairs(energyTypes) do
+        --
+        RegisterStatusbar('Player Bar ' .. n, [[Unitframe\UI-HUD-UnitFrame-Player-PortraitOn-Bar-]] .. n)
+    end
+
+    -- target
+    RegisterStatusbar('Target Health', [[Unitframe\UI-HUD-UnitFrame-Target-PortraitOn-Bar-Health]])
+    RegisterStatusbar('Target Health Status', [[Unitframe\UI-HUD-UnitFrame-Target-PortraitOn-Bar-Health-Status]])
+    RegisterStatusbar('Target Health Mask', [[Unitframe2x\uiunitframetargethealthmask2x]])
+    -- RegisterStatusbar('Player Health Mask2', [[Unitframe2x\plunderstormplayerhealthbarmask2x]])
+    RegisterStatusbar('Target Mana Mask', [[Unitframe2x\uiunitframetargetmanamask2x]])
+
+    for i, n in ipairs(energyTypes) do
+        --
+        RegisterStatusbar('Target Bar ' .. n, [[Unitframe\UI-HUD-UnitFrame-Target-PortraitOn-Bar-]] .. n)
+    end
 end
-
--- target
-RegisterStatusbar('Target Health', [[Unitframe\UI-HUD-UnitFrame-Target-PortraitOn-Bar-Health]])
-RegisterStatusbar('Target Health Status', [[Unitframe\UI-HUD-UnitFrame-Target-PortraitOn-Bar-Health-Status]])
-RegisterStatusbar('Target Health Mask', [[Unitframe2x\uiunitframetargethealthmask2x]])
--- RegisterStatusbar('Player Health Mask2', [[Unitframe2x\plunderstormplayerhealthbarmask2x]])
-RegisterStatusbar('Target Mana Mask', [[Unitframe2x\uiunitframetargetmanamask2x]])
-
-for i, n in ipairs(energyTypes) do
-    --
-    RegisterStatusbar('Target Bar ' .. n, [[Unitframe\UI-HUD-UnitFrame-Target-PortraitOn-Bar-]] .. n)
-end
-
 -- generator functions
 
 function Helper:CreateSharedMediaStatusBarGenerator(IsSelected, SetSelected)
